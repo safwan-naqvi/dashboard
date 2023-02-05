ae7bfd12ff7889976205c6927fb351e8f2c0fbcb
Safwan Abbas <safwanabbas2020@gmail.com>
2023-02-05 16:02:12 +0500

    Homepage done!

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -15,14 +15,14 @@ const Ecommerce = () => {
     <div className="mt-12">
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
         <div
-          className="bg-white dark:text-grey-200
+          className="bg-white dark:text-gray-200
         dark:bg-secondary-dark-bg h-44 rounded-xl
         w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat
         bg-cover bg-center"
         >
           <div className="flex justify-between items-center">
             <div>
-              <p className="text-grey-400 font-bold">Earnings</p>
+              <p className="text-gray-400 font-bold">Earnings</p>
               <p className="text-2xl">$72,334.78</p>
             </div>
           </div>
@@ -42,7 +42,7 @@ const Ecommerce = () => {
             <div
               key={item.title}
               className="bg-white 
-              dark:text-grey-200
+              dark:text-gray-200
               dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
             >
               <button
@@ -58,13 +58,82 @@ const Ecommerce = () => {
                   {item.percentage}
                 </span>
               </p>
-              <p className="text-sm text-grey-400 mt-1">{item.title}</p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
             </div>
           ))}
         </div>
       </div>
       {/* section two revenue */}
-      <div></div>
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div
+          className="bg-white
+        dark:text-gray-200 dark:bg-secondary-dark-bg
+        m-3 p-4 rounded-2xl md:w-780"
+        >
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl cursor-pointer">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl cursor-pointer">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          {/* Left section of revenue */}
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p className="items-center flex">
+                  <span className="text-3xl font-semibold">$98,342</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    25%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              {/* left side of revenue */}
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold">$48,342</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+
+              {/* chart */}
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
